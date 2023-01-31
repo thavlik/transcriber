28bc459d784234316ee81fca502b98b30feaa80e
Tom Havlik <thavlik@protonmail.com>
2023-01-31 01:20:51 -0600

    ...

--- imgsearch/pkg/cache/data/s3/set.go
+++ imgsearch/pkg/cache/data/s3/set.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/pkg/errors"
 
@@ -16,21 +17,23 @@ func (c *s3DataCache) Set(
 	img *search.Image,
 	r io.Reader,
 ) error {
-	if _, err := s3manager.NewUploader(c.sess).
-		UploadWithContext(ctx, &s3manager.UploadInput{
-			Bucket: aws.String(c.bucket),
-			Key:    aws.String(EncodeHash(img.Hash())),
-			Body:   r,
-			ACL:    aws.String("public-read"),
-		}); err != nil {
-		// Warning: there may be a dangling multipart upload.
-		// This is bad because s3 charges for the storage they consume,
-		// even if the object is never visible. We can clean them up,
-		// but in the event multiple users are uploading the same file
-		// at the same time, we may end up deleting a valid upload.
-		// This is a rare case, so we'll just ignore it for now.
-		// The workaround is manually free multipart uploads on a
-		// regular basis.
+	key := aws.String(EncodeHash(img.Hash()))
+	if resp, err := s3manager.NewUploader(c.sess).
+		UploadWithContext(
+			ctx,
+			&s3manager.UploadInput{
+				Bucket: aws.String(c.bucket),
+				Key:    key,
+				Body:   r,
+				ACL:    aws.String("public-read"),
+			},
+		); err != nil {
+		// Try and clean up the dangling multipart upload
+		_, _ = c.s3.AbortMultipartUpload(&s3.AbortMultipartUploadInput{
+			Bucket:   aws.String(c.bucket),
+			Key:      key,
+			UploadId: aws.String(resp.UploadID),
+		})
 		return errors.Wrap(err, "s3")
 	}
 	return nil
